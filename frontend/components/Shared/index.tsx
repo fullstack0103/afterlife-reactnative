5a64ab4b7fd93db69468e23df4644bfe6c5a8d8c
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2022-01-26 22:05:48 +0800

    update

--- frontend/components/Shared/index.tsx
+++ frontend/components/Shared/index.tsx
@@ -3,12 +3,14 @@ import AButton from './AButton'
 import AIcon from './AIcon'
 import AText from './AText'
 import BlockChainSelector from './BlockChainSelector'
+import BottomSheet from './BottomSheet'
 
 export {
   ABottomPopup,
   AButton,
   AIcon,
   AText,
+  BottomSheet,
 
   BlockChainSelector,
 }
